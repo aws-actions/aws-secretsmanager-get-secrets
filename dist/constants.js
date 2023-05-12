a804051ef802433a3b4aaf505809e83b2476eaa1
flmeyer <florian.meyer@capgemini.com>
2023-05-12 14:15:44 +0200

    Update compiled js files

--- dist/constants.js
+++ dist/constants.js
@@ -1,5 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.CLEANUP_NAME = exports.LIST_SECRETS_MAX_RESULTS = void 0;
+exports.LIST_SECRETS_MAX_RESULTS = void 0;
 exports.LIST_SECRETS_MAX_RESULTS = 100;
-exports.CLEANUP_NAME = 'SECRETS_LIST_CLEAN_UP';
